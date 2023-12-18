192fc76195317923dc7ae0dcef517de682e8e2d4
Huy Nguyễn <huyntp1103@gmail.com>
2023-12-18 14:18:12 +0700

    Add HomeController

---
+++ Mango.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = SD.RoleAdmin)]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
